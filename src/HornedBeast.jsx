556da23e82f2d0d4a533e1f6fadfe592609ff685
Jessica Palacios <jpyamakawa@yahoo.com>
2024-08-15 23:40:21 -0400

    lab 02

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,10 +1,23 @@
-function HornedBeast(props){
+import { useState } from "react";
+
+
+
+function HornedBeast(props) {
+    const [clicks, setClicks] = useState(0);
+
+    function reaccionClick() {
+        console.log("Fav clicked " + props.title)
+        setClicks(clicks + 1);
+    }
+    
+
     return (
         <>
         <h2>{props.title}</h2>
         <img src={props.imageUrl} style={{width:'80px'}} alt={props.title} />
         <p>Descripcion: {props.description}</p>
-        <p>mi edad {props.edad}</p>
+        <button onClick={reaccionClick}>fav - {clicks}</button>
+        {/* <p>mi edad {props.edad}</p> */}
         </>
     );
 }
