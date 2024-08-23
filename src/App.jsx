26490e5cb1691eb7ae9d9abfa6062bdd217fec78
Jessica Palacios <jpyamakawa@yahoo.com>
2024-08-23 00:12:56 -0400

    commit lab 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import SelectBeast from './SelectBeast';
 import Header from './Header';
 import Gallery from './Gallery';
 import Footer from './Footer';
-
+import {useState}from 'react';
+import HornedBeast from './hornedBeast.json';
 
 function App() {
-  
 
+  const [displayModal, setDisplayModal] = useState (false);
+  const [selectedBeast, setSelectedBeast] = useState ({});
+  
+  function mostrarModal(name){
+    const beastWithName =HornedBeast.find(beast=>beast.title===name);
+    setSelectedBeast(beastWithName);
+    setDisplayModal(true);
+  }
+
+  console.log (selectedBeast);
   return (
     <>
-      <Header />
-      <Gallery />
-      <Footer />
-
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <h1>Hola soy Jessica</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      */}
+      <Header/>
+      <Gallery mostrarModal={mostrarModal} allbeast={HornedBeast}/>
+      <SelectBeast selectedBeast={selectedBeast} show={displayModal} mostrarModal={mostrarModal}/>
+      <Footer/>
     </> 
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,9 +1,9 @@
 import HornedBeast from './HornedBeast';
-import hornedBeasts from './hornedBeast.json';
+import hornedBeast from './hornedBeast.json';
 import {Container} from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 
-function Gallery() {
+function Gallery(props) {
     return (
         <>
             {/* <HornedBeast title="imagen1" description="Esta es una imagen de referencia"
@@ -12,10 +12,14 @@ function Gallery() {
                 imageUrl="https://plus.unsplash.com/premium_photo-1680831748238-1fe52ad0e3af?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */}
             <Container>
                 <Row xs={2} md={4} Lg={6}>
-            {hornedBeasts.map(hornedBeast => (
-                <HornedBeast key={hornedBeast._id} title={hornedBeast.title} description={hornedBeast.description} imageUrl={hornedBeast.image_url}/>
+            {/* {hornedBeasts.map(hornedBeast => (
+                <HornedBeast key={hornedBeast._id} title={hornedBeast.title} description={hornedBeast.description} imageUrl={hornedBeast.image_url}/> 
             ))
-            }
+            } */}
+            {props.allbeast.map(data2=> (
+                <HornedBeast mostrarModal={props.mostrarModal} key={data2._id} title={data2.title} description={data2.description} horns={data2.horns} keyword={data2.image_url}/>
+            ))
+        }
             </Row>
             </Container>
         </>
